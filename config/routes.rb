e599b4ed1297da9e2ecfe3bd922a4ed37443256c
Andrey <napalkov@yahoo.com>
2021-08-02 11:00:59 -0400

    login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get "/", to: "hike#index"
+  get "users/:id", to: "users#show"
+  get "hikes/:id", to: "hikes#show"
+  post "signup", to "users#create"
+  post "login", to "sessions#create"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
