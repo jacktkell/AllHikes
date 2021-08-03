5678d2bb5cd9977664ebf7980c6f2e661b6db57a
Andrey <napalkov@yahoo.com>
2021-08-03 12:59:09 -0400

    front end fix

--- client/src/components/HikeCollection.js
+++ client/src/components/HikeCollection.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import styled from 'styled-components';
+import styled from "styled-components";
 import{Box} from './style';
 import HikeCard from './HikeCard'
 
