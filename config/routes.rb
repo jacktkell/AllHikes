40104ddc55c3b4992f5ff93e763be594be4cd190
Andrey <napalkov@yahoo.com>
2021-08-03 10:24:21 -0400

    fix postgres

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "/", to: "hike#index"
   get "users/:id", to: "users#show"
   get "hikes/:id", to: "hikes#show"
-  post "signup", to "users#create"
-  post "login", to "sessions#create"
+  post "signup", to: "users#create"
+  post "login", to: "sessions#create"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_08_02_142025) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id_id"
+    t.bigint "hike_id_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hike_id_id"], name: "index_comments_on_hike_id_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+  end
+
+  create_table "hikes", force: :cascade do |t|
+    t.float "length"
+    t.string "name"
+    t.float "x_coordinate"
+    t.float "y_coordinate"
+    t.float "elevation_gain"
+    t.string "image_url"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "hike_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
