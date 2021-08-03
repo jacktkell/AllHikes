bf8a91f095591a418c58fbc051df5102a8873034
Andrey <napalkov@yahoo.com>
2021-08-03 11:20:16 -0400

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,9 @@ Hike.create(name: 'Craggy Pinnacle', length: 1.1, elevation_gain: 230, x_coordin
 Hike.create(name: 'Three Top Mountain', length: 4.7, elevation_gain: 1492, x_coordinate: 36.42188, y_coordinate: -81.59793, image_url: "https://www.hikingupward.com/NCSP/ThreeTopMountain/images/MapSmall.gif")
 Hike.create(name: 'Chimney Top', length: 5.4, elevation_gain: 1960, x_coordinate: 38.98159, y_coordinate: -79.23113, image_url: "https://www.hikingupward.com/MNF/ChimneyTop/images/MapSmall.gif")
 Hike.create(name: "Strickler Knob", length: 5.8, elevation_gain: 1050, x_coordinate: 38.69143, y_coordinate: -78.58004, image_url: "https://www.hikingupward.com/GWNF/StricklerKnob/images/MapSmall.gif")
-
+Hike.create(name: "North Mountain", length: 6.8, elevation_gain: 1154, x_coordinate: 37.81906, y_coordinate: -79.63468, image_url: "https://www.hikingupward.com/GWNF/NorthMountainPetesCave/images/MapSmall.gif")
+Hike.create(name: "Sky Meadows", length: 7.1, elevation_gain: 1292, x_coordinate: 38.99144, y_coordinate: -77.96591, image_url: "https://www.hikingupward.com/OVH/SkyMeadows/images/MapSmall.jpg")
+Hike.create(name: 'Max Patch Mountain', length:7.1, elevation_gain: 2061, x_coordinate: 35.77318, y_coordinate: -82.9957, image_url: "https://www.hikingupward.com/PNF/MaxPatch/images/MapSmall.gif")
 
 # puts "Creating Trips..."
 # Trip.create(user_id: 1, hike_id: 1)
