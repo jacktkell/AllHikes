78e5d14a10e6ca57549c1a9f5f736a9ba3fa9bc6
jack <jack.kelling7@gmail.com>
2021-08-05 15:29:15 -0500

    sign up fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
     # skip_before_action :authorize, only: :create
 
-    def create 
-        user = User.create!(user_params)
-        session[:user_id] = user.id 
-        render json: user, status: :created
-    end
+    def create
+        user = User.create(user_params)
+        if user.valid?
+             render json: {id:user.id, name: user.name}
+        else 
+             render json: {message: user.errors.full_messages}
+        end 
+     end 
     
 
     def show
